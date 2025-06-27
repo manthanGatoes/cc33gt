be9cee77e04703d503e24ad5dedea0a4dd02e1e4
manthanGatoes <manthan.manral@gatoes.com>
2025-06-27 20:27:06 +0530

    allow all origin for testing

--- src/main/java/com/org/ticketing/support/config/CorsConfig.java
+++ src/main/java/com/org/ticketing/support/config/CorsConfig.java
@@ -13,6 +13,6 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
